5228c69e63414ed0f724343ecf502d6dbb698674
carla <cfpcarla@gmail.com>
2020-03-09 22:03:18 -0400

    Implement case if city was not found

--- app.js
+++ app.js
@@ -22,11 +22,15 @@ app.get("/", (request, response) => {
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.city}&key=${apiKey}`)
     .then(res => res.json())
     .then((locationResponse) => {
-      const {
-        lat,
-        lng
-      } = locationResponse.results[0].geometry.location;
-      response.json({ latitude: lat, longitude: lng });
+      if (locationResponse.status === 'ZERO_RESULTS') {
+        response.status(404).json({ error: 'City not found' })
+      } else {
+        const {
+          lat,
+          lng
+        } = locationResponse.results[0].geometry.location;
+        response.json({ latitude: lat, longitude: lng });
+      }
     })
     .catch(function (error) {
       console.log({ error });
